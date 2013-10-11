68f2e1a8d8e19f471da0429fe15a7c0d7d4bad32
Robert Dunn <rcdunnii@gmail.com>
2013-10-11 11:23:45 -0500

    serve image node.js

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,31 +1,87 @@
-var querystring = require("querystring");
+var requestHandler  = {},
+    url             = require( "url" ),
+    qs              = require( "querystring" ),
+    fs              = require( "fs" ),
+    formidable      = require( "formidable" ),
+    mime            = require( "mime" )
+;
 
-function start(response, postData) {
-    console.log("Request Handler 'Start' Called");
 
-    var body = '<html>' +
-        '<head>' +
-        '<meta http-equiv="Content-Type" content="text/html; '+        
-        'charset=UTF-8" />'+
-        '</head>'+
-        '<body>'+
-        '<form action="/upload" method="post">'+
-        '<textarea name="text" rows="20" cols="60"></textarea>'+
-        '<input type="submit" value="Submit Text" />'+
-        '</form>'+
-        '</body>'+
-        '</html>';
-        response.writeHead(200, {"Content-Type" : "text/html"});
-        response.write(body);
-        response.end();   
-}
+requestHandler.start = function( response ) {
+    var body = '<html>\n'+
+               '<head>\n'+
+                '\t<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />\n'+
+                '\t<title>Welcome to the nodejs tutorial</title>\n'+
+                '</head>\n'+
+                '<body>\n'+
+                '\t<form action="/upload" method="post" enctype="multipart/form-data">\n' +
+                '\t\t<input type="file" name="upload" />\n' +
+                '\t\t<input type="submit" value="Upload File" />\n' +
+                '\t</form>\n'+
+                '</body>\n'+
+                '</html>\n';
+        
+    console.log( "Request for 'start' is called." );
+                
+    response.writeHead( 200, { "Content-Type" : "text/html" } );
+    response.end( body );
+};
 
-function upload(response, postData) {
-        console.log("Request Handler 'Upload' Called");
-        response.writeHead(200, {"Content-Type" : "text/plain"});
-        response.write("You've sent the text: " + querystring.parse(postData).text);
-        response.end();        
-}
+requestHandler.upload = function( response, request ) {
+    console.log( "Request for 'upload' is called." );
+        
+    var form    = new formidable.IncomingForm();
 
-exports.start = start;
-exports.upload = upload;  
+    console.log( "Preparing upload" );
+
+
+    form.parse( request, function( error, fields, files ){
+        console.log( "Completed Parsing" );
+
+        if( error ){
+            response.writeHead( 500, { "Content-Type" : "text/plain" } );
+            response.end( "CRAP! " + error + "\n" );
+            return;
+        }
+
+        fs.renameSync( files.upload.path, "/Temp/" + files.upload.name );
+
+        response.writeHead( 200, { "Content-Type" : "text/html" } );
+        response.write( "received image <br />" );
+        response.end( "<img src='/show?i=" + files.upload.name + "' />" );
+                
+    });
+};
+
+requestHandler.show = function( response, request ) {
+    console.log( "Request handler 'show' was called. " );
+        
+    var image = qs.parse( url.parse( request.url ).query ).i;
+
+    if( !image ){
+        response.writeHead( 500, { "Content-Type" : "text/plain" } );
+        response.end( " No Image in QueryString ");
+        return;
+    }
+
+    fs.readFile( "/tmp/" + image, "binary", function( error, file ) {
+        var type = "";
+
+        if( error ){
+            response.writeHead( 500, { "Content-Type" : "text/plain" } );
+            response.end( error + "\n" );
+            return;
+         }
+                
+        type = mime.lookup( file );
+
+        response.writeHead( 200, { "Content-Type" : type } );
+        response.end( file, "binary" );
+
+    });
+};
+
+
+exports.start   = requestHandler.start;
+exports.upload  = requestHandler.upload;
+exports.show    = requestHandler.show;
